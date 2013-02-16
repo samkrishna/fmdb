e136a6c4656391cc79038a3433f22a611feb5dcf
Sam Krishna <sam.krishna@gmail.com>
2013-02-16 00:05:50 -0800

    Updated code to support NSDecimalNumber extraction for high-precision purposes.

--- src/FMResultSet.m
+++ src/FMResultSet.m
@@ -368,10 +368,10 @@
     id returnValue = nil;
     
     if (columnType == SQLITE_INTEGER) {
-        returnValue = [NSNumber numberWithLongLong:[self longLongIntForColumnIndex:columnIdx]];
+        returnValue = [NSDecimalNumber numberWithLongLong:[self longLongIntForColumnIndex:columnIdx]];
     }
     else if (columnType == SQLITE_FLOAT) {
-        returnValue = [NSNumber numberWithDouble:[self doubleForColumnIndex:columnIdx]];
+        returnValue = [NSDecimalNumber numberWithDouble:[self doubleForColumnIndex:columnIdx]];
     }
     else if (columnType == SQLITE_BLOB) {
         returnValue = [self dataForColumnIndex:columnIdx];
